e88fa5e6d0aadfb993b54525024f09216d2ff0c0
DylanV <dverrezen@gmail.com>
2017-02-20 21:44:37 +0200

    Greedy bandit tests now use nosetests

--- test_bandit_greedy.py
+++ test_bandit_greedy.py
@@ -1,14 +1,19 @@
 from bandit_greedy import Bandit_Greedy
 import numpy as np
 
-greedy_bandit = Bandit_Greedy(3)
+def test_greedy_bandit():
 
-means = [0.3,0.5,0.7]
-std_devs = [0.1, 0.1, 0.1]
+    greedy_bandit = Bandit_Greedy(3)
 
-for turn in range(1000):
-    choice = greedy_bandit.get_choice()
-    reward = np.clip(np.random.normal(means[choice],std_devs[choice]),0,1)
-    greedy_bandit.update(choice, reward)
+    means = [0.3,0.5,0.7]
+    std_devs = [0.1, 0.1, 0.1]
+    choice_counts = {0:0,1:0,2:0}
 
-print(greedy_bandit._means)
+    for turn in range(1000):
+        choice = greedy_bandit.get_choice()
+        reward = np.clip(np.random.normal(means[choice],std_devs[choice]),0,1)
+        greedy_bandit.update(choice, reward)
+        choice_counts[choice] += 1
+
+    assert(greedy_bandit._means.argmax() == 2)
+    assert (choice_counts[2] > choice_counts[1] and choice_counts[2] > choice_counts[0])
