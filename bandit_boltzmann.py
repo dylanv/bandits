45b5f4d100427febe512c84fccbdbd3bece7c47e
DylanV <dverrezen@gmail.com>
2017-02-20 21:58:25 +0200

    Tests and syntax

--- bandit_boltzmann.py
+++ bandit_boltzmann.py
@@ -1,20 +1,21 @@
 import numpy as np
 
-class Bandit_Boltzmann:
+
+class BanditBoltzmann:
 
     def __init__(self, num_arms, temp=0.1):
         self._num_arms = num_arms
         self._temp = temp
 
         self._arms = [i for i in range(self._num_arms)]
-        self._means = [0.5 for i in range(self._num_arms)]
 
+        self._means = np.zeros(self._num_arms) + 0.5
 
-        self._probabilities = [0.0 for i in range(self._num_arms)]
+        self._probabilities = np.zeros(self._num_arms)
         self._calc_probabilities()
 
-        self._reward_totals = [0 for i in range(self._num_arms)]
-        self._choice_totals = [0 for i in range(self._num_arms)]
+        self._reward_totals = np.zeros(self._num_arms)
+        self._choice_totals = np.zeros(self._num_arms)
 
     def get_choice(self):
         return np.random.choice(self._arms, p=self._probabilities)
@@ -35,7 +36,8 @@ class Bandit_Boltzmann:
             self._probabilities[i] = np.exp(self._means[i] / self._temp) / denom_sum
             total += self._probabilities[i]
 
-
         for i in range(self._num_arms):
             self._probabilities[i] = self._probabilities[i] / total
 
+    def get_probabilities(self):
+        return self._probabilities
--- test_bandit_boltzmann.py
+++ test_bandit_boltzmann.py
@@ -1,14 +1,16 @@
-from bandit_boltzmann import Bandit_Boltzmann
+from bandit_boltzmann import BanditBoltzmann
 import numpy as np
 
-boltzmann_bandit = Bandit_Boltzmann(3, temp=0.01)
 
-means = [0.3,0.5,0.7]
-std_devs = [0.1, 0.1, 0.1]
+def test_boltzmann_bandit():
+    boltzmann_bandit = BanditBoltzmann(3, temp=0.01)
 
-for turn in range(1000):
-    choice = boltzmann_bandit.get_choice()
-    reward = np.clip(np.random.normal(means[choice],std_devs[choice]),0,1)
-    boltzmann_bandit.update(choice, reward)
+    means = [0.3, 0.5, 0.7]
+    std_devs = [0.1, 0.1, 0.1]
 
-print(boltzmann_bandit._probabilities)
+    for turn in range(1000):
+        choice = boltzmann_bandit.get_choice()
+        reward = np.clip(np.random.normal(means[choice], std_devs[choice]), 0, 1)
+        boltzmann_bandit.update(choice, reward)
+
+    assert(boltzmann_bandit.get_probabilities().argmax() == 2)
